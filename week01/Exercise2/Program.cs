dab27c1ddacd95ee2870ed75c9f0f74085906bcd
morpho2005 <digitalchristian05@gmail.com>
2025-01-11 13:22:26 -0700

    comm

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string userinput = Console.ReadLine();
+        int usergrade = int.Parse(userinput);
+        char letter;
+
+        if (usergrade >= 90) {
+            letter = 'A';
+        } else if (usergrade >= 80) {
+            letter = 'B';
+        } else if (usergrade >= 70) {
+            letter = 'C';
+        } else if (usergrade >= 60) {
+            letter = 'D';
+        } else {
+            letter = 'F';
+        }
+
+        if (usergrade >= 70) {
+            Console.WriteLine($"You got a {letter}, that means you pass!");
+        } else {
+            Console.WriteLine($"You got a {letter}, this means you failed, too bad.");
+        }
     }
 }
