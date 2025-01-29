fe519a15d2af8b59652b93502f4742270d136221
morpho2005 <digitalchristian05@gmail.com>
2025-01-29 10:02:58 -0700

    commit

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+        
     }
 }
---
+++ week04/YouTubeVideos/video.cs
@@ -0,0 +1,14 @@
+class Video{
+    string _title;
+    string _author;
+    int _seconds;
+    List<Comment> _comments = new List<Comment>();
+    public Video(string title, string author, int seconds){ 
+		_title = title; 
+		_author = author; 
+		_seconds = seconds; 
+	} 
+    public void AddComment(string author, string comment){
+        _comments.Add(new Comment(author, comment));
+    }
+}
